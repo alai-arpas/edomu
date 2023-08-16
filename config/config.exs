35cf8176c22b6c19afe229730587611103b91477
Andrea Lai <alai.arpas@gmail.com>
2023-08-16 07:55:40 +0200

    url and arpos config

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,15 @@ import Config
 
 config :arpos, :windows_share, System.get_env("WINDOWS_SHARE")
 
+config :arpos, Arpos.Repo,
+  username: System.get_env("SUPACLOUD_DB_USER"),
+  password: System.get_env("SUPACLOUD_DB_PASSWORD"),
+  hostname: System.get_env("SUPACLOUD_DB_HOST_CARG"),
+  database: System.get_env("SUPACLOUD_DB_NAME"),
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
 config :edomu,
   ecto_repos: [Edomu.Repo]
 
