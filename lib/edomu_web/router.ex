f0184da083ed6c6b8ad7d4caf6c052aefa330346
Andrea Lai <alai.arpas@gmail.com>
2023-10-17 18:18:17 +0200

    register

--- lib/edomu_web/router.ex
+++ lib/edomu_web/router.ex
@@ -126,7 +126,6 @@ defmodule EdomuWeb.Router do
 
     live_session :redirect_if_user_is_authenticated,
       on_mount: [{EdomuWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
       live "/users/log_in", UserLoginLive, :new
       live "/users/reset_password", UserForgotPasswordLive, :new
       live "/users/reset_password/:token", UserResetPasswordLive, :edit
